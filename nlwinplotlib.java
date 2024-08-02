4cb55767b0b6e9f61020f3a1f0fb82928adf5863
Simeon Petkov <petkov.moni@gmail.com>
2024-08-02 13:58:26 +0300

    Updated library

--- nlwinplotlib.java
+++ nlwinplotlib.java
@@ -1,5 +1,6 @@
 package build;
 
+import data.Array;
 import data.ObjType;
 import data.ReadableFile;
 import data.WritableFile;
@@ -76,11 +77,37 @@ public enum nlwinplotlib implements Operation {
         @Override
         public void execute(Object[] instruction, float[] memory, HashMap<String, WritableFile> writableFiles, HashMap<String, ReadableFile> readableFiles, HashMap<String, data.Array> arrays, String[] stringTable) throws IOException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException {
             nlwinplotlib.window.points.clear();
+
+            nlwinplotlib.window.lineStarts.clear();
+            nlwinplotlib.window.lineEnds.clear();
         }
 
         @Override
         public String help() {
             return "Clears the window";
+        }
+    },
+    LINE {
+        @Override
+        public ObjType[] getArguments() {
+            return new ObjType[]{ObjType.NUMBER, ObjType.NUMBER, ObjType.NUMBER, ObjType.NUMBER};
+        }
+
+        @Override
+        public void execute(Object[] instruction, float[] memory, HashMap<String, WritableFile> writableFiles, HashMap<String, ReadableFile> readableFiles, HashMap<String, Array> arrays, String[] stringTable) throws IOException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException {
+            nlwinplotlib.window.lineStarts.add(new Point(
+                    Math.round(Interpreter.getValue(instruction[1], memory)),
+                    Math.round(Interpreter.getValue(instruction[2], memory))
+            ));
+            nlwinplotlib.window.lineEnds.add(new Point(
+                    Math.round(Interpreter.getValue(instruction[3], memory)),
+                    Math.round(Interpreter.getValue(instruction[4], memory))
+            ));
+        }
+
+        @Override
+        public String help() {
+            return "Draws a line from (arg0, arg1) to (arg2, arg3)";
         }
     };
 
@@ -100,6 +127,9 @@ public enum nlwinplotlib implements Operation {
             case "CLEARWIN":
                 return CLEARWIN;
 
+            case "LINE":
+                return LINE;
+
             default:
                 return null;
         }
@@ -110,9 +140,14 @@ public enum nlwinplotlib implements Operation {
 
     class Window extends JPanel {
         public java.util.List<Point> points;
+        public java.util.List<Point> lineStarts;
+        public java.util.List<Point> lineEnds;
 
         public Window(String title, int sizeX, int sizeY) {
             points = new ArrayList<>();
+
+            lineStarts = new ArrayList<>();
+            lineEnds = new ArrayList<>();
 
             JFrame frame = new JFrame(title);
             frame.setSize(sizeX, sizeY);
@@ -129,6 +164,10 @@ public enum nlwinplotlib implements Operation {
 
             for (int i = 0; i < points.size(); i ++) {
                 g.fillRect(points.get(i).x, points.get(i).y, 5, 5);
+            }
+
+            for (int i = 0; i < lineStarts.size(); i ++){
+                g.drawLine(lineStarts.get(i).x, lineStarts.get(i).y, lineEnds.get(i).x, lineEnds.get(i).y);
             }
 
             repaint();
